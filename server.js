c77c0f800a79a02664121732e2c6aa458e357d78
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-28 15:56:12 -0400

    basic GET / POST reqs

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.MYSQL_PW,
-    database: 'todos'
-  });
+const mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PW,
+  database: "todos",
+});
 
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+mysqlConnection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + mysqlConnection.threadId);
+});
+
+app.post("/todo", (req, res) => {
+  mysqlConnection.query(
+    `INSERT INTO todoitem (label, note, completed) VALUES ('${req.body.label}', '${req.body.note}', '${req.body.completed}')`
+  );
+
+  res.send("Data Inserted");
+});
+
+app.get("/todos", (req, res) => {
+  console.log(req.body);
+
+  mysqlConnection.query(
+    `SELECT * FROM todoitem`,
+    function (err, results, fields) {
+      console.log(results);
+      console.log(fields);
     }
-   
-    console.log('connected as id ' + connection.threadId);
-  });
+  );
+
+  res.send();
+});
+
+app.listen(3000, () => {
+  console.log("Running Express Server on Port 3000...");
+});
